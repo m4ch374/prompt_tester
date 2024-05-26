3f28f9b0d7b9005343c0bd621e4d037b69e0b4b9
m4ch374 <spiner47@gmail.com>
2024-05-26 18:53:04 +1000

    feat(access_token): get access token from google

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,39 @@
-from typing import Union
-
+import requests
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import dotenv_values
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: str, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/auth")
+def read_item(access_code: str):
+    env_var = dotenv_values(".env")
+    resp = requests.post(
+        url="https://oauth2.googleapis.com/token",
+        data={
+            "client_id": env_var["GOOGLE_CLIENT_ID"],
+            "client_secret": env_var["GOOGLE_CLIENT_SECRET"],
+            "code": access_code,
+            "grant_type": "authorization_code",
+            "redirect_uri": "http://localhost:3000",
+        },
+        timeout=1
+    )
+
+    data = resp.json()
+    print(data)
+
+    return {"access_code": access_code}
